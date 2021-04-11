6b47646aebf424873cd17119efb7afd684ad42a5
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-04-11 15:40:11 +0300

    Add array of time + Function generates random numbers

--- Js/app.js
+++ Js/app.js
@@ -1,6 +1,12 @@
 'User strict';
 
 
+let total= 0;
+let time = ['6:00AM','7:00AM','8:00AM','9:00AM','10:00AM','11:00AM','12:00PM','1:00PM','2:00PM','3:00PM','4:00PM','5:00PM','6:00PM','7:00PM','8:00PM']
+function numCust(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 
 const Seattle = {
   minCust: 23,
