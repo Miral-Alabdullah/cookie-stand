761995da348330e1e52ec67fe1c8526fd51b50c1
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-04-18 06:04:27 +0300

    Editing the open hours

--- Js/app2.js
+++ Js/app2.js
@@ -7,8 +7,6 @@ let headings = ['Locations','Address', 'Open Hours'];
 let address = ['(206) 443-9398 2600 2nd Ave #803 Seattle, Washington(WA), 9812',
   '4-23-7 Shindou, Matsubara, Osaka', '4-23-7 Shindou, Matsubara, Osaka',
   'Sheikh Mohammed Bin Rashed Blvd PO Box 123234', 'Calle de la Prosa, 104 San Borja, Lima-Perú'];
-let openingHours = ['6:00AM','7:00AM','8:00AM','9:00AM','10:00AM','11:00AM','12:00PM',
-  '1:00PM','2:00PM','3:00PM','4:00PM','5:00PM','6:00PM','7:00PM'];
 
 
 let home = document.getElementById('section');
@@ -80,5 +78,5 @@ for (let i = 0; i<locations.length; i++){
   rowData.textContent = address[i];
   rowData = document.createElement('td');
   rowLocations.appendChild(rowData);
-  rowData.textContent = openingHours[i];
+  rowData.textContent = '6:00AM - 7:00PM';
 }
